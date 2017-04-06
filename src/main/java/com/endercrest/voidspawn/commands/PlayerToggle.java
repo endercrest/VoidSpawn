8b705dcd6ae1f40bcc688ef3b22ab390bd5a9fa4
ShadowDragon7015 <junkkitty12@gmail.com>
2017-04-05 22:16:17 -0500

    Fixed Player Toggle Formatting

--- src/main/java/com/endercrest/voidspawn/commands/PlayerToggle.java
+++ src/main/java/com/endercrest/voidspawn/commands/PlayerToggle.java
@@ -5,43 +5,43 @@ import org.bukkit.entity.Player;
 import com.endercrest.voidspawn.VoidSpawn;
 
 public class PlayerToggle implements SubCommand
-  {
-   VoidSpawn plugin;
-   public PlayerToggle(VoidSpawn instance){
-	this.plugin = instance;
-   }
-   public boolean onCommand(Player p, String[] args)
-   {
-     if(p.hasPermission("vs.player.toggle")){
-				if(this.plugin.Toggle.containsKey(p.getUniqueId())){
-					if(this.plugin.Toggle.get(p.getUniqueId()) == false){
-						this.plugin.Toggle.put(p.getUniqueId(), true);
-						p.sendMessage("Toggled teleport to: On");
-					}
-					else{
-						p.sendMessage("Toggled teleport to: Off");
-						this.plugin.Toggle.put(p.getUniqueId(), false);
-					}
+
+{
+	VoidSpawn plugin;
+	public PlayerToggle(VoidSpawn instance){
+		this.plugin = instance;
+	}
+	public boolean onCommand(Player p, String[] args)
+	{
+		if(p.hasPermission("vs.player.toggle")){
+			if(this.plugin.Toggle.containsKey(p.getUniqueId())){
+				if(this.plugin.Toggle.get(p.getUniqueId()) == false){
+					this.plugin.Toggle.put(p.getUniqueId(), true);
+					p.sendMessage("Toggled teleport to: On");
 				}
 				else{
 					p.sendMessage("Toggled teleport to: Off");
 					this.plugin.Toggle.put(p.getUniqueId(), false);
 				}
-				return true;
-		     }
-			 else{
-				return false;
-			 }
-   }
-   
-   public String helpInfo()
-   {
-     return "/vs toggle - Turns off void teleport.";
-   }
-   
-   public String permission()
-   {
-     return "vs.player.toggle";
-   }
+			}
+			else{
+				p.sendMessage("Toggled teleport to: Off");
+				this.plugin.Toggle.put(p.getUniqueId(), false);
+			}
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 
+	public String helpInfo()
+	{
+		return "/vs toggle - Turns off void teleport.";
+	}
+
+	public String permission()
+	{
+		return "vs.player.toggle";
+	}
 }
