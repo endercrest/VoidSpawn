03a08bb526ac8315e5f1ec356bfc4449137798e3
endercrest <tcvs@endercrest.com>
2017-05-27 14:52:26 -0400

    Add playername variable
    Also added the ability to run commands as the console. This allows for commands to be ran even if the player doesn't have the permissions.

--- src/main/java/com/endercrest/voidspawn/modes/Command.java
+++ src/main/java/com/endercrest/voidspawn/modes/Command.java
@@ -2,6 +2,7 @@ package com.endercrest.voidspawn.modes;
 
 import com.endercrest.voidspawn.ConfigManager;
 import com.endercrest.voidspawn.VoidSpawn;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class Command implements SubMode {
@@ -13,8 +14,19 @@ public class Command implements SubMode {
         String[] commands = commandString.split(";");
         boolean success = true;
         for(String command : commands){
-            boolean b = player.performCommand(command.trim());
-            if(!b)
+            command = command.replace("${playername}", player.getName());
+            boolean status;
+
+            String[] perms = command.split(":", 2);
+            //Check if cmd needs to be ran as OP/Console
+            if(perms.length > 1 && perms[0].equalsIgnoreCase("op")){
+                String cmd = perms[1];
+                status = Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.trim());
+            }else{
+                status = player.performCommand(command.trim());
+            }
+
+            if(!status)
                 success = false;
         }
         if(!success){
