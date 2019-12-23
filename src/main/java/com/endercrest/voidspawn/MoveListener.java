9416127d2ff9d0f8b03d03e31764c1da8d878a1a
YellowZaki <algoca99@gmail.com>
2019-12-23 16:58:48 +0100

    Fix falling from trapdoors

--- src/main/java/com/endercrest/voidspawn/MoveListener.java
+++ src/main/java/com/endercrest/voidspawn/MoveListener.java
@@ -1,5 +1,6 @@
 package com.endercrest.voidspawn;
 
+import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,8 +15,13 @@ public class MoveListener implements Listener {
     @EventHandler
     public void onMoveEvent(PlayerMoveEvent event){
         Player player = event.getPlayer();
-        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().isSolid()){
+        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().isSolid() && !isTrapDoor(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType())){
             TeleportManager.getInstance().setPlayerLocation(player.getUniqueId(), player.getLocation());
         }
     }
+    
+    public boolean isTrapDoor(Material mat) {
+        return (mat == Material.OAK_TRAPDOOR || mat == Material.SPRUCE_TRAPDOOR || mat == Material.BIRCH_TRAPDOOR || mat == Material.JUNGLE_TRAPDOOR || mat == Material.ACACIA_TRAPDOOR || mat == Material.DARK_OAK_TRAPDOOR || mat == Material.IRON_TRAPDOOR);
+    }    
+    
 }
