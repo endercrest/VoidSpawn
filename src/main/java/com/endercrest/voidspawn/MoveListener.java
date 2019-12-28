7eca666c8d94313fe5035206bc1a31db43f9355b
YellowZaki <algoca99@gmail.com>
2019-12-28 03:44:39 +0100

    Made conflicting blocks detection future proof

--- src/main/java/com/endercrest/voidspawn/MoveListener.java
+++ src/main/java/com/endercrest/voidspawn/MoveListener.java
@@ -1,5 +1,6 @@
 package com.endercrest.voidspawn;
 
+import org.bukkit.Tag;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
@@ -15,13 +16,13 @@ public class MoveListener implements Listener {
     @EventHandler
     public void onMoveEvent(PlayerMoveEvent event){
         Player player = event.getPlayer();
-        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().isSolid() && !isTrapDoor(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType())){
+        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType().isSolid() && !isConflictingBlock(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType())){
             TeleportManager.getInstance().setPlayerLocation(player.getUniqueId(), player.getLocation());
         }
     }
     
-    public boolean isTrapDoor(Material mat) {
-        return (mat == Material.OAK_TRAPDOOR || mat == Material.SPRUCE_TRAPDOOR || mat == Material.BIRCH_TRAPDOOR || mat == Material.JUNGLE_TRAPDOOR || mat == Material.ACACIA_TRAPDOOR || mat == Material.DARK_OAK_TRAPDOOR || mat == Material.IRON_TRAPDOOR);
+    public boolean isConflictingBlock(Material mat) {
+        return Tag.TRAPDOORS.isTagged(mat);
     }    
     
 }
