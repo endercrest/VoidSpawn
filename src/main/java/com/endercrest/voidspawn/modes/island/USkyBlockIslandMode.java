076d285a277515d98e8264946b8701055648f222
Thomas Cordua-von Specht <tcvs@endercrest.com>
2021-01-18 23:16:46 -0800

    Add back USkyBlock support

--- src/main/java/com/endercrest/voidspawn/modes/island/USkyBlockIslandMode.java
+++ src/main/java/com/endercrest/voidspawn/modes/island/USkyBlockIslandMode.java
@@ -3,12 +3,29 @@ package com.endercrest.voidspawn.modes.island;
 import com.endercrest.voidspawn.TeleportResult;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import us.talabrek.ultimateskyblock.api.IslandInfo;
+import us.talabrek.ultimateskyblock.api.uSkyBlockAPI;
 
 public class USkyBlockIslandMode extends BaseIslandMode {
+    private final uSkyBlockAPI usb;
+
+    public USkyBlockIslandMode() {
+        this.usb = (uSkyBlockAPI) Bukkit.getPluginManager().getPlugin("uSkyBlock");
+    }
 
     @Override
     public TeleportResult onActivateIsland(Player player, String worldname) {
-        return null;
+        IslandInfo info = usb.getIslandInfo(player);
+        if (info.getWarpLocation() != null) {
+            player.teleport(info.getWarpLocation());
+            return TeleportResult.SUCCESS;
+        }
+        if (info.getIslandLocation() != null) {
+            player.teleport(info.getIslandLocation());
+            return TeleportResult.SUCCESS;
+        }
+
+        return TeleportResult.MISSING_ISLAND;
     }
 
     @Override
