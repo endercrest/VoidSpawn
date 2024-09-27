d7e5cd33146577a259c9bd957759e5aa5513b6c6
Thomas Cordua-von Specht <tcvs@endercrest.com>
2024-09-26 22:55:03 -0700

    Add info to SuperiorSkyblock status

--- src/main/java/com/endercrest/voidspawn/modes/island/SuperiorSkyblockIslandMode.java
+++ src/main/java/com/endercrest/voidspawn/modes/island/SuperiorSkyblockIslandMode.java
@@ -4,12 +4,16 @@ import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
 import com.bgsoftware.superiorskyblock.api.island.Island;
 import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import com.endercrest.voidspawn.TeleportResult;
+import com.endercrest.voidspawn.modes.status.Status;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-public class SuperiorSkyblockIslandMode extends BaseIslandMode{
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class SuperiorSkyblockIslandMode extends BaseIslandMode {
     @Override
     public boolean isEnabled() {
 
@@ -26,7 +30,7 @@ public class SuperiorSkyblockIslandMode extends BaseIslandMode{
 
         Location location = island.getTeleportLocation(World.Environment.NORMAL);
         if (location == null) {
-            for (World.Environment env: World.Environment.values()) {
+            for (World.Environment env : World.Environment.values()) {
                 location = island.getTeleportLocation(env);
                 if (location != null) break;
             }
@@ -38,6 +42,16 @@ public class SuperiorSkyblockIslandMode extends BaseIslandMode{
         }
 
         return TeleportResult.MISSING_ISLAND;
+    }
+
+    @Override
+    public Status[] getStatus(String worldName) {
+        boolean isSuperiorConfiguredCorrect = !SuperiorSkyblockAPI.getSettings().getVoidTeleport().isMembers() && !SuperiorSkyblockAPI.getSettings().getVoidTeleport().isVisitors();
+
+        return Stream.concat(
+                Arrays.stream(super.getStatus(worldName)),
+                Stream.of(new Status(isSuperiorConfiguredCorrect ? Status.Type.COMPLETE : Status.Type.INCOMPLETE, "SuperiorSkyblock's 'void-teleport' disabled"))
+        ).toArray(Status[]::new);
     }
 
     public static boolean isModeEnabled() {
